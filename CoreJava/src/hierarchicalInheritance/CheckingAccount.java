6fe77264f254a8e2d602dea982bd3de7b0e5da45
MOHAN <MOHAN@LAPTOP-P82JCG8J>
2024-11-21 12:30:23 +0530

    Shopping Online Inheritance

--- CoreJava/src/hierarchicalInheritance/CheckingAccount.java
+++ CoreJava/src/hierarchicalInheritance/CheckingAccount.java
@@ -1,4 +1,4 @@
-package inheritance;
+package hierarchicalInheritance;
 
 public class CheckingAccount extends BankAccount
 {
--- CoreJava/src/hierarchicalInheritance/SavingAccount.java
+++ CoreJava/src/hierarchicalInheritance/SavingAccount.java
@@ -1,4 +1,4 @@
-package inheritance;
+package hierarchicalInheritance;
 
 public class SavingAccount extends BankAccount
 {
